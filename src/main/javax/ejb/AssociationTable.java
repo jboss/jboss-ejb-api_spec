7cd2e84b7c30af21bcb52be18e039829aac386ed
Bill Burke <bburke@redhat.com>
2004-10-07 15:48:52 +0000

    RUNTIME
    git-svn-id: https://svn.jboss.org/repos/jbossas/trunk/jboss-ejb3x@24359 84be2c1e-ba19-0410-b317-a758671a6fc1

--- src/main/javax/ejb/AssociationTable.java
+++ src/main/javax/ejb/AssociationTable.java
@@ -2,14 +2,17 @@
 //EJB Specification Copyright 2004 Sun Microsystems, Inc.
 package javax.ejb;
 
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
 
 import static java.lang.annotation.ElementType.*;
 
 /**
  * @author Emmanuel Bernard
  */
-@Target({METHOD, FIELD})
+@Target({METHOD, FIELD})  @Retention(RUNTIME)
 public @interface AssociationTable
 {
    Table table() default @Table(specified = false);
