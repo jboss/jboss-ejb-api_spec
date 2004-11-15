8de4f4eda73fe9bc94ef57d939216ce266a74cb5
Kabir Khan <kkhan@redhat.com>
2004-11-15 13:41:28 +0000

    Added annotation types for EJB3 callbacks
    git-svn-id: https://svn.jboss.org/repos/jbossas/trunk/jboss-ejb3x@25899 84be2c1e-ba19-0410-b317-a758671a6fc1

---
+++ src/main/javax/ejb/CallbackListener.java
@@ -0,0 +1,21 @@
+/*
+ * JBoss, the OpenSource EJB server
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package javax.ejb;
+
+import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+
+/**
+ * @author <a href="mailto:kabir.khan@jboss.org">Kabir Khan</a>
+ * @version $Revision$
+ */
+@Target({TYPE}) @Retention(RUNTIME)
+public @interface CallbackListener {
+   String value(); // fully qualified name of the callback listenerclass
+}
