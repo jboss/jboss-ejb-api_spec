d914307c3dbe151312bb66a174c8ec4fb45406ae
Bill Burke <bburke@redhat.com>
2005-02-09 05:03:39 +0000

    update callbacks to EDR2
    git-svn-id: https://svn.jboss.org/repos/jbossas/trunk/jboss-ejb3x@28452 84be2c1e-ba19-0410-b317-a758671a6fc1

---
+++ src/main/javax/persistence/PrePersist.java
@@ -0,0 +1,24 @@
+/*
+ * JBoss, the OpenSource J2EE webOS
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package javax.persistence;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * comment
+ *
+ * @author <a href="mailto:bill@jboss.org">Bill Burke</a>
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PrePersist
+{
+}
