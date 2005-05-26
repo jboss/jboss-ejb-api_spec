162512b68c47bf7004a27fb4cdf8ec8ac5e542c1
Bill DeCoste <wdecoste@redhat.com>
2005-05-26 02:25:42 +0000

    support for @Init
    git-svn-id: https://svn.jboss.org/repos/jbossas/trunk/jboss-ejb3x@31555 84be2c1e-ba19-0410-b317-a758671a6fc1

---
+++ src/main/javax/ejb/Init.java
@@ -0,0 +1,23 @@
+/*
+ * JBoss, the OpenSource J2EE webOS
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package javax.ejb;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to mark a method that will initialize a stateful session
+ *
+ * @author <a href="mailto:bill@jboss.org">Bill Burke</a>
+ * @version $Revision$
+ */
+@Target({ElementType.METHOD}) @Retention(RetentionPolicy.RUNTIME)
+public @interface Init{
+   String[] value() default {};
+}
