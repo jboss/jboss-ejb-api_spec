c62ee12e3d1c1235528e9885987d7b94e5e07ed6
Bill Burke <bburke@redhat.com>
2005-05-26 22:52:20 +0000

    remove @Remote and @Local.  Replace with @RemoteInterface and @LocalInterface
    git-svn-id: https://svn.jboss.org/repos/jbossas/trunk/jboss-ejb3x@31600 84be2c1e-ba19-0410-b317-a758671a6fc1

--- src/main/javax/ejb/LocalInterface.java
+++ src/main/javax/ejb/LocalInterface.java
@@ -1,5 +1,5 @@
 /*
- * JBoss, the OpenSource EJB server
+ * JBoss, the OpenSource J2EE webOS
  *
  * Distributable under LGPL license.
  * See terms of license at gnu.org.
@@ -12,10 +12,14 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * Bean class annotation that specifies remote interfaces of the Session bean.
+ *
  * @author <a href="mailto:bill@jboss.org">Bill Burke</a>
  * @version $Revision$
  */
-@Target(ElementType.TYPE) @Retention(RetentionPolicy.RUNTIME)
-public @interface Remote
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LocalInterface
 {
+   Class[] value();
 }
